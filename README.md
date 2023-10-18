ee1e7c83056ff91529a24e373e8bfa675a2103d0
Shawn Meyer <shawn.meyer@Associatedbank.com>
2023-10-18 08:23:42 -0500

    fix: pull request fixes
    Signed-off-by: Shawn Meyer <shawn.meyer@Associatedbank.com>

--- README.md
+++ README.md
@@ -52,6 +52,11 @@ Set to `true` to prevent failed tests from failing the job.
 Set to `false` or leave blank to fail the job if there are any failed tests (recommended).
 <br/>Default: `false`
 
+#### `show-failed-tests-only`
+**Optional** - Boolean flag. 
+Set to `true` to show only the failed tests. This is useful if you you have many tests and the results exceed the markdown comment limit in github
+Set to `false` or leave blank to show all the test results (recommended).
+<br/>Default: `false`
 ## Outputs
 
 #### `tests-total`
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
 import { processTestResults } from './results';
-import { processTestCoverage } from './coverage';
-import { getInputs, publishComment, setFailed, setSummary } from './utils';
-import { formatCoverageMarkdown, formatResultMarkdown } from './formatting/markdown';
-import { formatCoverageHtml, formatResultHtml, formatTitleHtml } from './formatting/html';
+import { setFailed, setSummary } from './utils';
+import { formatResultMarkdown } from './formatting/markdown';
+import { formatResultHtml, formatTitleHtml } from './formatting/html';
+import * as fs from 'fs';
 
 const run = async (): Promise<void> => {
   try {
-    const {
-      token,
-      title,
-      resultsPath,
-      coveragePath,
-      coverageType,
-      coverageThreshold,
-      postNewComment,
-      allowFailedTests,
-      onlyShowFailedTests,
-      showTestOutput
-    } = getInputs();
+    // const {
+    //   token,
+    //   title,
+    //   resultsPath,
+    //   coveragePath,
+    //   coverageType,
+    //   coverageThreshold,
+    //   postNewComment,
+    //   allowFailedTests,
+    //   onlyShowFailedTests,
+    //   showTestOutput
+    // } = getInputs();
+
+    const title = "a";
+    const resultsPath = './files/ab/_ip-10-8-96-225_2023-10-04_13_16_40.trx';
+    // const coveragePath = null;
+    // const token = 'a'
+    const allowFailedTests = true;
+    const onlyShowFailedTests = false;
+    const showTestOutput = true;
 
     let comment = '';
     let summary = formatTitleHtml(title);
@@ -26,14 +34,20 @@ const run = async (): Promise<void> => {
     comment += formatResultMarkdown(testResult);
     summary += formatResultHtml(testResult, onlyShowFailedTests, showTestOutput);
 
-    if (coveragePath) {
-      const testCoverage = await processTestCoverage(coveragePath, coverageType, coverageThreshold);
-      comment += testCoverage ? formatCoverageMarkdown(testCoverage, coverageThreshold) : '';
-      summary += testCoverage ? formatCoverageHtml(testCoverage) : '';
-    }
+    fs.writeFile("output.html", summary, function(err) {
+      if(err) {
+      return console.log(err);
+      }});
+    // if (coveragePath) {
+    //   const testCoverage = await processTestCoverage(coveragePath, coverageType, coverageThreshold);
+    //   comment += testCoverage ? formatCoverageMarkdown(testCoverage, coverageThreshold) : '';
+    //   summary += testCoverage ? formatCoverageHtml(testCoverage) : '';
+    // }
+
+    console.log(comment);
 
     await setSummary(summary);
-    await publishComment(token, title, comment, postNewComment);
+    //await publishComment(token, title, comment, postNewComment);
   } catch (error) {
     setFailed((error as Error).message);
   }
